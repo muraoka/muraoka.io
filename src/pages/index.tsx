19788a7ed4eb3be6446d65fcec17efebda4315e3
muraoka <taigamuraoka1998@gmail.com>
2020-01-04 22:12:35 +0900

    Add content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,23 @@
-import React from "react"
+import React from 'react';
 
-export default () => <div>Hello world!</div>
+const Index = () => {
+  return (
+    <main>
+      <h1>muraoka.io</h1>
+      <p>
+        <strong>GitHub</strong> :{' '}
+        <a href="https://github.com/muraoka">muraoka</a>
+        <br />
+        <strong>Twitter</strong> :{' '}
+        <a href="https://twitter.com/muraoka__">muraoka__</a>
+        <br />
+        <strong>Scrapbox</strong> :{' '}
+        <a href="https://scrapbox.io/taigamuraoka/">muraokaのScrapbox</a>
+        <br />
+        <strong>Mail</strong> : taigamuraoka1998@gmail.com
+      </p>
+    </main>
+  );
+};
+
+export default Index;
