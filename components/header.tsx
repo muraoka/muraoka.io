e03e9eba0067bf94df58497d0ccadca6bc1c2065
muraoka <taigamuraoka1998@gmail.com>
2020-11-23 18:38:08 +0900

    Add links

--- components/header.tsx
+++ components/header.tsx
@@ -6,24 +6,51 @@ const Header: React.FC = () => {
   const router = useRouter();
   const Tag = router.pathname === '/' ? 'h1' : 'p';
   return (
-    <>
-      <Tag>
+    <header>
+      <Tag className="title">
         <Link href="/">
           <a>muraoka.io</a>
         </Link>
       </Tag>
+      <div>
+        <p>
+          <a href="https://github.com/muraoka" target="_blank" rel="noreferrer">
+            GitHub
+          </a>
+        </p>
+        <p>
+          <a
+            href="https://twitter.com/muraoka_jp"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Twitter
+          </a>
+        </p>
+      </div>
 
       <style jsx>{`
-        ${Tag} {
+        header {
+          display: flex;
+          justify-content: space-between;
+          padding: 0 1rem 2rem 0;
+        }
+        .title {
           font-size: 1.5rem;
           font-weight: 600;
-          margin-bottom: 2rem;
+        }
+        div {
+          display: flex;
+          align-items: flex-end;
+        }
+        div > p {
+          padding-left: 10px;
         }
         a {
           color: #222;
         }
       `}</style>
-    </>
+    </header>
   );
 };
 export default Header;
